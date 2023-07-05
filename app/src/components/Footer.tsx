74a8453a2400b4a5ee12f7383be4fc259f27ee8b
Rodrigo <pittezr.dev@gmail.com>
2023-07-05 15:16:29 -0300

    feat: Footer section

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,7 +1,24 @@
 export function Footer() {
   return (
-    <>
-      <h2 className="underline font-bold text-4xl">Footer Test</h2>
-    </>
+    <footer className="bg-neutral-950 text-white">
+      <div className="container mx-auto flex items-center justify-between flex-col md:flex-row max-w-5xl p-4 text-sm">
+        <p className="flex-grow">
+          &copy; {new Date().getFullYear()} &middot; All rights reserved.
+        </p>
+        <ul className="flex flex-nowrap gap-2 text-center">
+          <li>
+            <a href="#" className="link font-light">
+              Terms of service
+            </a>
+          </li>
+          <li className="text-gray-500">&middot;</li>
+          <li>
+            <a href="#" className="link font-light">
+              Privacy policy
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 }
